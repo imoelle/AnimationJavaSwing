6b12a25668df5ba43e7602ae5f1f8b5499a20035
IngoW <moeller.ingo@gmx.de>
2020-12-03 13:04:02 +0100

    Main - calculate some test data

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import waterworld.WaterWorld;
 import waterworld.utilities.PVector2D;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  * Main.java <br><br>
  *
@@ -10,5 +12,16 @@ import waterworld.utilities.PVector2D;
 public class Main {
     public static void main(String[] args) {
         new WaterWorld();
+
+        for (int i = 0; i < 50; i++) {
+            double test = ThreadLocalRandom.current().nextDouble();
+            double test2 = ThreadLocalRandom.current().nextDouble(0, 3);
+            System.out.println(i + 1 + ". (" + (test*100 % 3) + ", " + test2 + ")");
+        }
+
     }
+
+
+
+
 }
